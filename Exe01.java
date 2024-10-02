08406616b752bf7c43e1bd109492755cf65291fb
Digbijaya18swain <gobindaswain0802@gmail.com>
2024-10-02 21:32:35 +0530

    I am only Practising

---
+++ Exe01.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Exe01 {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.add(23);
+        st.add(24);
+    }
+    
+}
